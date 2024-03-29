1593bc953b98d53f365b2c7e71ebebd7a7d9b441
gaeun0123 <cheun0928@naver.com>
2024-03-29 14:16:54 +0900

    양옆, 원거리 촬영 추가

--- GUI/py/Camera.py
+++ GUI/py/Camera.py
@@ -2,23 +2,31 @@ from  PyQt5.QtCore import *
 import cv2
 import numpy as np
 import time
+import threading
+from Loading import Loading
 
 class Camera(QThread):                         
     update = pyqtSignal(np.ndarray)
+    finishedRecording = pyqtSignal()
     
     def __init__(self, parent=None):
         super().__init__()
         self.isRunning = False # 카메라 동작 플래그
         self.recording = False # 녹화 시작 플래그
         self.name = 'gaeun'
+        self.mode = 'close' # 첫 촬영은 근접촬영
+        self.recordingCount = 0 #녹화 순서 지정을 위한 횟수 추적
+    
     
     def run(self):
         self.video = cv2.VideoCapture(-1)
+        
         if not self.video.isOpened():
             print("카메라를 열 수 없습니다.")
             return
         
         self.isRunning = True
+        
         while self.isRunning:
             ret, frame = self.video.read()
             if ret:
@@ -26,18 +34,51 @@ class Camera(QThread):
                 if self.recording:
                     # 녹화가 활성화된 경우 프레임 저장
                     timestamp = int(time.time() * 2000)  # 밀리초 단위 타임스탬프
-                    filename = f'/home/addinedu/git_ws/deeplearning-repo-6/GUI/data/face/{self.name}_frame_{timestamp}.jpg'
+                    
+                    # 근접 촬영인지 떨어져서 촬영인지 판단
+                    if self.mode == 'close':
+                        filename = f'/home/addinedu/git_ws/deeplearning-repo-6/GUI/data/face/{self.mode}_{self.name}_frame_{timestamp}.jpg'
+                    elif self.mode == 'far':
+                        filename = f'/home/addinedu/git_ws/deeplearning-repo-6/GUI/data/face/{self.mode}_{self.name}_frame_{timestamp}.jpg'
+                    else:
+                        print("해당 모드가 없습니다.")
+                        break
+                    
                     cv2.imwrite(filename, frame)
+                    
             time.sleep(0.1)
         
         
     def stop(self):
+        if self.recording: #녹화 중지
+            self.stopRecording()
+            
         self.isRunning = False #카메라 동작 중지
         self.video.release()
     
+    
     def startRecording(self):
         self.recording = True #녹화 시작
+        self.recordingCount += 1
+        
+        if self.recordingCount == 1:
+            threading.Timer(10, self.stopRecording).start() # 10초 녹화
+        elif self.recordingCount == 2:
+            self.mode = 'far'
+            threading.Timer(10, self.stop).start() # 녹화 종료
+        else:
+            self.stop() #촬영이 끝나면 카메라, 녹화 종료
+        
         
     def stopRecording(self):
         self.recording = False #녹화 중지
+        self.finishedRecording.emit()
     
+    
+    # 카운트다운이 끝나면 모드변경 후 촬영 재개
+    def recMode(self, signal):
+        self.mode = 'far'
+        print(f"모드 변경: {self.mode}, 녹화 재개")
+        
+        self.startRecording()
+        
