54b3ea67d03e1395814942bee5e0ed7d4026bcbe
djy0404 <dydrltk1919@naver.com>
2024-04-02 16:40:53 +0900

    feature: Login.py, Main_window.py, SR.py

---
+++ GUI/py/Login.py
@@ -0,0 +1,36 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.uic import loadUi
+
+class LoginWindow(QWidget):
+    def __init__(self,main_window):
+        super().__init__()
+        # UI 파일 로드
+        self.main_window = main_window
+        
+        self.resize(476,470)
+
+        self.ui = loadUi('/home/djy0404/amr_ws/project/communication_model/test_files/GUI/Login.ui', self)
+        # 버튼 클릭 이벤트 핸들러 연결
+        self.Btn_Login.clicked.connect(self.login_clicked)
+
+    def login_clicked(self):
+        # 로그인 버튼 클릭 시 수행할 작업 정의
+        # 예시로 당신의 이름과 비밀번호를 출력하는 작업 수행
+        name = self.ui.Txt_Name.toPlainText()
+        password = self.ui.Txt_Pw.toPlainText()
+        print("이름:", name)
+        print("비밀번호:", password)
+        if name == "11" and password == "11":
+            self.main_window.show_Sr_page()  # MainWindow 객체의 show_Sr_page 메서드 호출
+    
+
+
+    
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = LoginWindow()
+    window.show()
+    sys.exit(app.exec_())
---
+++ GUI/py/Main_window.py
@@ -0,0 +1,66 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QStackedWidget, QWidget
+from PyQt5.QtGui import QPixmap
+from PyQt5 import uic
+from Login import LoginWindow
+from SR import ChatModule
+from userSetting import UserSetting
+
+
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.main_page = uic.loadUi('/home/djy0404/amr_ws/project/communication_model/test_files/GUI/Main_window.ui')
+
+        self.stacked_widget = QStackedWidget(self)  # QStackedWidget 인스턴스 생성
+        self.setCentralWidget(self.stacked_widget)  # MainWindow의 중앙 위젯으로 설정
+
+        self.stacked_widget.addWidget(self.main_page)  # Main 페이지를 stacked widget에 추가
+        main_widget_size = self.size()
+        self.resize(main_widget_size)
+
+        self.stacked_widget.setCurrentWidget(self.main_page)  # 처음에 Main 페이지를 보여줌
+
+        self.main_page.Btn_Login.clicked.connect(self.show_login_page)  # 로그인 페이지로 전환
+        self.main_page.Btn_regist.clicked.connect(self.show_regist_page)  # 사용자등록 페이지로 전환
+
+        
+        image_path = '/home/djy0404/amr_ws/project/communication_model/test_files/GUI/robot.png'
+        self.show_image(image_path)
+
+    
+    def show_image(self, image_path):
+        pixmap = QPixmap(image_path)  # 이미지 파일을 QPixmap으로 변환합니다.
+        pixmap = pixmap.scaled(self.main_page.Label_Image.size(), aspectRatioMode=False)
+        self.main_page.Label_Image.setPixmap(pixmap)  # QLabel에 이미지를 설정합니다.
+    
+    
+    def show_login_page(self):
+        self.login_page = LoginWindow(self)
+        self.stacked_widget.addWidget(self.login_page)  # Login 페이지를 stacked widget에 추가
+        self.stacked_widget.setCurrentWidget(self.login_page)  # Login 페이지를 보여줌
+
+
+    def show_Sr_page(self):
+        self.Sr_page = ChatModule(self) 
+        self.stacked_widget.addWidget(self.Sr_page) #SR 페이지를 stacked widget에 추가
+        self.stacked_widget.setCurrentWidget(self.Sr_page)
+
+    def show_regist_page(self):
+        self.user_setting_page = UserSetting(self) 
+        self.stacked_widget.addWidget(self.user_setting_page) #SR 페이지를 stacked widget에 추가
+        self.stacked_widget.setCurrentWidget(self.user_setting_page)
+
+    def closeEvent(self, event):
+        if hasattr(self.Sr_page, 'closeEvent'):
+            self.Sr_page.closeEvent(event)
+        event.accept()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+
+    window.show()
+    sys.exit(app.exec_())
