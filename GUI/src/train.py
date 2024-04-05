08d5e7c8975a1ec7d5627be930927b73f8dfa72b
gaeun0123 <cheun0928@naver.com>
2024-04-05 14:07:56 +0900

    Feat Face train model 훈련, 성공 여부 코드 추가

--- GUI/src/train.py
+++ GUI/src/train.py
@@ -4,34 +4,42 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5 import uic
 from PyQt5.QtCore import *
-import cv2
 import os
-from PIL import Image
-import threading
+from GUI.src.DesignManager import DesignManager
 
 from GUI.src.Loading import Loading # GIF
-from Face_recognize.face_save_learn import FaceImageCollectorAndRecognizerTrainer # Face 학습 model
+from Face_recognize.face_save_learn import FaceTrainer # Face 학습 model
 
 
 from_class = uic.loadUiType("GUI/ui/train.ui")[0]
 data_path = "GUI/data/face"
 model_save_path = "Face_recognize/model"
 
+
 class TrainClass(QMainWindow, from_class) :
-    
-    def __init__(self,parent = None):
-        super().__init__(parent)
+     
+    def __init__(self, main_window):
+        super().__init__()
         self.setupUi(self)
         
-        self.setWindowTitle("사용자 등록 중입니다.")
-        
-        self.loadingInstance = None
         self.createDirectory(model_save_path)
         
-        self.faceRecognizer = FaceImageCollectorAndRecognizerTrainer(data_path, model_save_path)
+        self.main_window = main_window
+        
+        self.initUI()
+        # Face recognize model train
+        self.train()
+        
+        # Change window
+        self.successBtn.clicked.connect(self.changeWindow)
+        
+        
+    def initUI(self):
+        self.designManager = DesignManager()
+        self.setWindowTitle("사용자 등록 중입니다.")
         self.successBtn.hide()
         self.initGIF()
-    
+     
     
     # 폴더 생성
     def createDirectory(self, directory):
@@ -60,7 +68,27 @@ class TrainClass(QMainWindow, from_class) :
         self.movie.start()
         
         self.show()
+    
+    
+    # 훈련 시작
+    def train(self):
+        self.faceRecognizer = FaceTrainer(data_path, model_save_path)
+        self.faceRecognizer.train_model()
         
+        # Train Success Signal
+        self.faceRecognizer.trainingCompleted.connect(self.trainSuccess)
+    
+    # 훈련 완료 판단
+    def trainSuccess(self):
+        self.movie.stop() # GIF 종료
+        self.successBtn.show() # 화면 전환 버튼 활성화
+        self.successBtn.setText("Home")
+        self.label.setText("당신만의 'Tier'가 생성되었습니다.")
+    
+    # main 페이지로 이동
+    def changeWindow(self):
+        self.main_window.show_main_page()
+    
     
 if __name__ == "__main__":
     app = QApplication(sys.argv)
