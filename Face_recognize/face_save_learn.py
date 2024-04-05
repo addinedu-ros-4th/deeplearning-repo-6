35c670f5b3f26ea6fc69ced96ab32d31f6265a3a
gaeun0123 <cheun0928@naver.com>
2024-04-05 16:06:07 +0900

    Refactor train 완료 시그널 발생

--- Face_recognize/face_save_learn.py
+++ Face_recognize/face_save_learn.py
@@ -3,18 +3,21 @@ import cv2
 import numpy as np
 import yaml
 import cvlib as cv
-from PyQt5.QtCore import QObject, pyqtSignal # GUI 통신
+from PyQt5.QtCore import QThread, pyqtSignal # PyQt 시그널 정의
+
 
 # Face Recognize train
-class FaceTrainer(QObject):
-    trainingCompleted = pyqtSignal(bool)    # 훈련 완료 시그널
+class FaceTrainer(QThread):
+    trainCompleted = pyqtSignal(bool) # 훈련 완료 시그널
     
     def __init__(self, image_folder, model_save_path):
+        super().__init__()
         self.image_folder = image_folder
         self.model_save_path = model_save_path
         self.recognizer = cv2.face.LBPHFaceRecognizer_create()
         self.label_dict = {}  # 사용자 이름과 정수 라벨을 매핑하는 딕셔너리
-
+        
+        
     # 데이터 준비 (얼굴)
     def prepare_training_data(self):
         faces = []
@@ -62,40 +65,43 @@ class FaceTrainer(QObject):
                 yaml.dump(self.label_dict, file, allow_unicode=True)
 
             print("학습 완료")
-            
-
-    def check_training_success(self):
-        model_path = os.path.splitext(self.model_save_path)[0] + 'labels.yaml'
-        
-        # 모델 파일 및 라벨 파일이 모두 존재하는지 확인
-        if os.path.exists(self.model_save_path) and os.path.exists(model_path):
-            # 모델 파일이 정상적으로 읽어지는지 확인
-            try:
-                self.recognizer.read(self.model_save_path)
-            except Exception as e:
-                print(f"Error loading model: {e}")
-                return False
-            
-            # 라벨 파일이 정상적으로 읽어지는지 확인
-            try:
-                with open(model_path, 'r', encoding='utf-8') as file:
-                    self.label_dict = yaml.safe_load(file)
-            except Exception as e:
-                print(f"Error loading label file: {e}")
-                return False
-            
-            self.trainingCompleted.emit(True)   # 성공 시그널 전달
-            return True
-
+            self.trainCompleted.emit(True)  # 학습 성공 시그널 발생
         else:
-            self.trainingCompleted.emit(False)  # 실패 시그널 전달
-            return False
-    
-    
+            self.trainCompleted.emit(False)  # 학습 실패 또는 불가능 시    
+
+
     # 훈련하기 전 라벨, 이미지 확인
     def possible_train(self, faces, label):
         if len(label) > 0 and len(faces) > 0:
             return True
         else:
             print("훈련 데이터가 충분하지 않습니다.")
-            return False      
+            return False
+    
+        
+    # def check_training_success(self):
+    #     model_path = os.path.splitext(self.model_save_path)[0] + 'labels.yaml'
+        
+    #     # 모델 파일 및 라벨 파일이 모두 존재하는지 확인
+    #     if os.path.exists(self.model_save_path) and os.path.exists(model_path):
+    #         # 모델 파일이 정상적으로 읽어지는지 확인
+    #         try:
+    #             self.recognizer.read(self.model_save_path)
+    #         except Exception as e:
+    #             print(f"Error loading model: {e}")
+    #             return False
+            
+    #         # 라벨 파일이 정상적으로 읽어지는지 확인
+    #         try:
+    #             with open(model_path, 'r', encoding='utf-8') as file:
+    #                 self.label_dict = yaml.safe_load(file)
+    #         except Exception as e:
+    #             print(f"Error loading label file: {e}")
+    #             return False
+            
+    #         self.trainingCompleted.emit(True)   # 성공 시그널 전달
+    #         return True
+
+    #     else:
+    #         self.trainingCompleted.emit(False)  # 실패 시그널 전달
+    #         return False
