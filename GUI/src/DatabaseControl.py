898ba0712d388699ce7b820ceefeae9332bebfe9
gaeun0123 <cheun0928@naver.com>
2024-04-11 20:02:05 +0900

    🐛 Fix when Database User info find
    Edit DatabaseControl.py : find_elements return result

--- GUI/src/DatabaseControl.py
+++ GUI/src/DatabaseControl.py
@@ -147,13 +147,9 @@ class DatabaseManager:
             self.conn.close()
     
     
-<<<<<<< Updated upstream
-    
-=======
     def find_elements(self, name, password):
         query = "SELECT UserId, Name, Password from Users where Name = %s and (Password) = %s;"
         self.cur.execute(query, (name, password))
         result = self.cur.fetchone()
         self.close_connection()
-        return result is not None
->>>>>>> Stashed changes
+        return result
